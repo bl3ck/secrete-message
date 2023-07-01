4c4e4a1159fc2ae450cf5752700c57d4cb61a2bb
bl3ck <eugenbleck@gmail.com>
2023-07-01 16:13:31 +0100

    Homepage setup

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,17 @@ import { Link } from "react-router-dom";
 export default function Home(){
     return(
         <>
-            <h1>Home page</h1>
-            <Link to='/create-message'> Create message</Link>
+            <h1>Welcome to Secrete Messages</h1>
+            <div className="text">
+                <p>Secrete Messages is an app which you can use to send message with unique links to anyone. </p>
+                <p>These messages expire after a set interval(Days, Hours or Views)</p>
+                <ul>
+                    <li>Days - Expires after x days</li>
+                    <li>Hours - Expires after x hours</li>
+                    <li>Views - Expires after x views</li>
+                </ul>
+                <p>To begin, click on create message</p>
+            </div>
         </>
     )
 }
